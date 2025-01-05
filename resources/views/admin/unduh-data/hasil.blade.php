85a61730d74168b13ca03c46632497961bdfa372
4mmang <ammangdeveloper@gmail.com>
2025-01-05 10:24:50 +0800

    [fix] perbaiki hasil unduhan

--- resources/views/admin/unduh-data/hasil.blade.php
+++ resources/views/admin/unduh-data/hasil.blade.php
@@ -34,99 +34,107 @@
                                 <td class="align-middle text-capitalize">{{ $pub->level }}</td>
                                 <td class="align-middle">
                                     @foreach ($pub->authors as $author)
-                                        {{ $author->nama }} @if (!$loop->last)
+                                        {{ $author->nama }}@if ($pub->authors->count() > 1)
+                                            <sup>{{ $loop->iteration }}</sup>
+                                        @endif
+                                        @if (!$loop->last)
                                             ,
                                         @endif
                                     @endforeach
                                 </td>
                             </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5" class="text-center">Data Kosong</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                            @empty
+                                <tr>
+                                    <td colspan="5" class="text-center">Data Kosong</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
 
+                </div>
             </div>
-        </div>
-        <div class="col-md-12 mt-3">
-            <h5>2. Data Penelitian</h5>
-            <div class="table-responsive">
-                <table class="table table-sm table-striped text-center table-bordered small align-middle">
-                    <thead class="text-center">
-                        <th>No</th>
-                        <th>Judul</th>
-                        <th>Tahun</th>
-                        <th>Level</th>
-                        <th>Authors</th>
-                    </thead>
-                    <tbody>
-                        @forelse ($penelitian as $pen)
-                            <tr>
-                                <td class="align-middle">{{ $loop->iteration }}</td>
-                                <td class="align-middle">{{ $pen->judul }}</td>
-                                <td class="align-middle">
-                                    {{ \Carbon\Carbon::parse($pen->tanggal_penelitian)->format('Y') }}</td>
-                                <td class="align-middle text-capitalize">{{ $pen->level }}</td>
-                                <td class="align-middle">
-                                    @foreach ($pen->authors as $author)
-                                        {{ $author->nama }} @if (!$loop->last)
-                                            ,
-                                        @endif
-                                    @endforeach
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5" class="text-center">Data Kosong</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-
-            </div>
-        </div>
-        <div class="col-md-12 mt-3">
-            <h5>3. Data Pengabdian</h5>
-            <div class="table-responsive">
-                <table class="table table-sm table-striped text-center table-bordered small align-middle">
-                    <thead class="text-center">
-                        <tr>
+            <div class="col-md-12 mt-3">
+                <h5>2. Data Penelitian</h5>
+                <div class="table-responsive">
+                    <table class="table table-sm table-striped text-center table-bordered small align-middle">
+                        <thead class="text-center">
                             <th>No</th>
                             <th>Judul</th>
                             <th>Tahun</th>
                             <th>Level</th>
                             <th>Authors</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($pengabdian as $pengab)
-                            <tr>
-                                <td class="align-middle">{{ $loop->iteration }}</td>
-                                <td class="align-middle">{{ $pengab->judul }}</td>
-                                <td class="align-middle">
-                                    {{ \Carbon\Carbon::parse($pengab->tanggal_pengabdian)->format('Y') }}</td>
-                                <td class="align-middle text-capitalize">{{ $pengab->level }}</td>
-                                <td class="align-middle">
-                                    @foreach ($pengab->authors as $author)
-                                        {{ $author->nama }} @if (!$loop->last)
-                                            ,
-                                        @endif
-                                    @endforeach
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5" class="text-center">Data Kosong</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @forelse ($penelitian as $pen)
+                                <tr>
+                                    <td class="align-middle">{{ $loop->iteration }}</td>
+                                    <td class="align-middle">{{ $pen->judul }}</td>
+                                    <td class="align-middle">
+                                        {{ \Carbon\Carbon::parse($pen->tanggal_penelitian)->format('Y') }}</td>
+                                    <td class="align-middle text-capitalize">{{ $pen->level }}</td>
+                                    <td class="align-middle">
+                                        @foreach ($pen->authors as $author)
+                                            {{ $author->nama }}@if ($pen->authors->count() > 1)
+                                                <sup>{{ $loop->iteration }}</sup>
+                                            @endif
+                                            @if (!$loop->last)
+                                                ,
+                                            @endif
+                                        @endforeach
+                                    </td>
+                                </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5" class="text-center">Data Kosong</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
 
-            </div>
-        </div>
-    </div>
-</body>
+                    </div>
+                </div>
+                <div class="col-md-12 mt-3">
+                    <h5>3. Data Pengabdian</h5>
+                    <div class="table-responsive">
+                        <table class="table table-sm table-striped text-center table-bordered small align-middle">
+                            <thead class="text-center">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Judul</th>
+                                    <th>Tahun</th>
+                                    <th>Level</th>
+                                    <th>Authors</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($pengabdian as $pengab)
+                                    <tr>
+                                        <td class="align-middle">{{ $loop->iteration }}</td>
+                                        <td class="align-middle">{{ $pengab->judul }}</td>
+                                        <td class="align-middle">
+                                            {{ \Carbon\Carbon::parse($pengab->tanggal_pengabdian)->format('Y') }}</td>
+                                        <td class="align-middle text-capitalize">{{ $pengab->level }}</td>
+                                        <td class="align-middle">
+                                            @foreach ($pengab->authors as $author)
+                                                {{ $author->nama }}@if ($pengab->authors->count() > 1)
+                                                    <sup>{{ $loop->iteration }}</sup>
+                                                    @endif @if (!$loop->last)
+                                                        ,
+                                                    @endif
+                                                @endforeach
+                                        </td>
+                                    </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="5" class="text-center">Data Kosong</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
 
-</html>
+                        </div>
+                    </div>
+                </div>
+            </body>
+
+            </html>
